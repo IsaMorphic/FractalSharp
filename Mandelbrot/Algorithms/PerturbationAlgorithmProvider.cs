8238b1189ee345d674f3dbad9f438a6376086678
Yodadude2003 <yodadude2003@gmail.com>
2019-01-25 08:15:01 -0600

    Modified how generics work for perturbation

--- Mandelbrot/Algorithms/PerturbationAlgorithmProvider.cs
+++ Mandelbrot/Algorithms/PerturbationAlgorithmProvider.cs
@@ -8,14 +8,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace Mandelbrot.Algorithms
 {
-    class PerturbationAlgorithmProvider<T> : GPUAlgorithmProvider<T>
+    class PerturbationAlgorithmProvider<T> : IAlgorithmProvider<T>
     {
         private IGenericMath<T> TMath;
         private ComplexMath<T> CMath;
-        private List<GenericComplex<T>> X, TwoX, A, B, C;
+        private List<Complex> X, TwoX, A, B, C;
 
         private CudaDeviceVariable<cuDoubleComplex> dev_points;
 
@@ -28,15 +29,20 @@ namespace Mandelbrot.Algorithms
         private T TwoPow10;
         private T NegTwoPow10;
 
-        private decimal center_real;
-        private decimal center_imag;
+        private T center_real;
+        private T center_imag;
 
         private int MaxIterations;
         private int SkippedIterations;
 
+        private double MagnitudeSquared(Complex a)
+        {
+            return a.Real * a.Real + a.Imaginary * a.Imaginary;
+        }
+
         // Perturbation Theory Algorithm, 
         // produces a list of iteration values used to compute the surrounding points
-        public override void Init(IGenericMath<T> TMath, decimal offsetX, decimal offsetY, int maxIterations)
+        public void Init(IGenericMath<T> TMath, decimal offsetX, decimal offsetY, int maxIterations)
         {
             this.TMath = TMath;
             CMath = new ComplexMath<T>(TMath);
@@ -51,77 +57,62 @@ namespace Mandelbrot.Algorithms
             TwoPow10 = TMath.fromInt32(1024);
             NegTwoPow10 = TMath.fromInt32(-1024);
 
-            center_real = offsetX;
-            center_imag = offsetY;
+            center_real = TMath.fromDecimal(offsetX);
+            center_imag = TMath.fromDecimal(offsetY);
 
-            A = new List<GenericComplex<T>>();
-            B = new List<GenericComplex<T>>();
-            C = new List<GenericComplex<T>>();
-            X = new List<GenericComplex<T>>();
-            TwoX = new List<GenericComplex<T>>();
+            A = new List<Complex>();
+            B = new List<Complex>();
+            C = new List<Complex>();
+            X = new List<Complex>();
+            TwoX = new List<Complex>();
 
             GetSurroundingPoints();
-            A.Add(new GenericComplex<T>(One, Zero));
-            B.Add(new GenericComplex<T>(Zero, Zero));
-            C.Add(new GenericComplex<T>(Zero, Zero));
+            A.Add(new Complex(1, 0));
+            B.Add(new Complex(0, 0));
+            C.Add(new Complex(0, 0));
             SkippedIterations = ApproximateSeries();
         }
 
         public void GetSurroundingPoints()
         {
-            decimal xn_r = center_real;
-            decimal xn_i = center_imag;
+            T xn_r = center_real;
+            T xn_i = center_imag;
 
             for (int i = 0; i < MaxIterations; i++)
             {
                 // pre multiply by two
-                decimal real = xn_r + xn_r;
-                decimal imag = xn_i + xn_i;
+                T real = TMath.Add(xn_r, xn_r);
+                T imag = TMath.Add(xn_i, xn_i);
 
-                decimal xn_r2 = xn_r * xn_r;
-                decimal xn_i2 = xn_i * xn_i;
+                T xn_r2 = TMath.Multiply(xn_r, xn_r);
+                T xn_i2 = TMath.Multiply(xn_i, xn_i);
 
-                GenericComplex<T> c = new GenericComplex<T>(TMath.fromDecimal(xn_r), TMath.fromDecimal(xn_i));
-                GenericComplex<T> two_c = new GenericComplex<T>(TMath.fromDecimal(real), TMath.fromDecimal(imag));
+                Complex c = new Complex(TMath.toDouble(xn_r), TMath.toDouble(xn_i));
+                Complex two_c = new Complex(TMath.toDouble(real), TMath.toDouble(imag));
 
                 X.Add(c);
                 TwoX.Add(two_c);
 
                 // calculate next iteration, remember real = 2 * xn_r
 
-                xn_r = xn_r2 - xn_i2 + center_real;
-                xn_i = real * xn_i + center_imag;
+                xn_r = TMath.Add(TMath.Subtract(xn_r2, xn_i2), center_real);
+                xn_i = TMath.Add(TMath.Multiply(real, xn_i), center_imag);
             }
         }
 
         private void IterateA(int n)
         {
-            A.Add(
-                CMath.Add(
-                    CMath.Multiply(
-                        CMath.Multiply(X[n - 1], A[n - 1]),
-                    Two),
-                new GenericComplex<T>(One, Zero)));
+            A.Add(2 * X[n - 1] * A[n - 1] + 1);
         }
 
         private void IterateB(int n)
         {
-            B.Add(
-                CMath.Add(
-                    CMath.Multiply(
-                        CMath.Multiply(X[n - 1], B[n - 1]),
-                    Two),
-                CMath.Multiply(A[n - 1], A[n - 1])));
+            B.Add(2 * X[n - 1] * B[n - 1] + A[n - 1] * A[n - 1]);
         }
 
         private void IterateC(int n)
         {
-            C.Add(
-                CMath.Multiply(
-                    CMath.Add(
-                        CMath.Multiply(X[n - 1], C[n - 1]),
-                        CMath.Multiply(A[n - 1], B[n - 1])),
-                Two));
+            C.Add(2 * X[n - 1] * C[n - 1] + 2 * A[n - 1] * B[n - 1]);
         }
 
         private int ApproximateSeries()
@@ -131,7 +122,7 @@ namespace Mandelbrot.Algorithms
                 IterateA(n);
                 IterateB(n);
                 IterateC(n);
-                if (TMath.LessThan(CMath.MagnitudeSquared(B[n]), CMath.MagnitudeSquared(C[n])))
+                if (MagnitudeSquared(B[n]) < MagnitudeSquared(C[n]))
                 {
                     return Math.Max(n - 3, 0);
                 }
@@ -142,7 +133,7 @@ namespace Mandelbrot.Algorithms
 
         // Non-Traditional Mandelbrot algorithm, 
         // Iterates a point over its neighbors to approximate an iteration count.
-        public override PixelData<T> Run(T x0, T y0)
+        public PixelData<T> Run(T x0, T y0)
         {
 
             // Get max iterations.  
@@ -152,23 +143,20 @@ namespace Mandelbrot.Algorithms
             int n = SkippedIterations;
 
             // Initialize some variables...
-            GenericComplex<T> zn;
+            Complex zn;
 
-            GenericComplex<T> d0 = new GenericComplex<T>(x0, y0);
+            Complex d0 = new Complex(TMath.toDouble(x0), TMath.toDouble(y0));
 
-            GenericComplex<T>[] d0_tothe = new GenericComplex<T>[4];
+            Complex[] d0_tothe = new Complex[4];
 
             d0_tothe[1] = d0;
 
             for (int i = 2; i < d0_tothe.Length; i++)
             {
-                d0_tothe[i] = CMath.Multiply(d0_tothe[i - 1], d0_tothe[1]);
+                d0_tothe[i] = d0_tothe[i - 1] * d0_tothe[1];
             }
 
-            GenericComplex<T> dn = CMath.Add(
-                CMath.Multiply(A[n], d0_tothe[1]),
-                CMath.Add(CMath.Multiply(B[n], d0_tothe[2]),
-                CMath.Multiply(C[n], d0_tothe[3])));
+            Complex dn = A[n] * d0_tothe[1] + B[n] * d0_tothe[2] + C[n] * d0_tothe[3];
 
 
             T znMagn = Zero;
@@ -178,69 +166,69 @@ namespace Mandelbrot.Algorithms
             {
 
                 // dn *= 2*Xn + dn
-                dn = CMath.Multiply(dn, CMath.Add(TwoX[n], dn));
+                dn *= TwoX[n] + dn;
 
                 // dn += d0
-                dn = CMath.Add(dn, d0);
+                dn += d0;
 
                 n++;
 
                 // zn = x[iter] * 0.5 + dn
-                zn = CMath.Add(X[n], dn);
+                zn = X[n] + dn;
 
-                znMagn = CMath.MagnitudeSquared(zn);
+                znMagn = TMath.fromDouble(MagnitudeSquared(zn));
 
             } while (TMath.LessThan(znMagn, TwoPow8) && n < maxIterations);
 
             return new PixelData<T>(znMagn, n, n < maxIterations);
         }
 
-        public override void GPUInit(CudaContext ctx, byte[] ptxImage, dim3 gridDim, dim3 blockDim)
-        {
-            gpuKernel = ctx.LoadKernelPTX(Resources.Kernel, "perturbation");
+    //    public override void GPUInit(CudaContext ctx, byte[] ptxImage, dim3 gridDim, dim3 blockDim)
+    //    {
+    //        gpuKernel = ctx.LoadKernelPTX(Resources.Kernel, "perturbation");
 
-            gpuKernel.GridDimensions = gridDim;
-            gpuKernel.BlockDimensions = blockDim;
-        }
+    //        gpuKernel.GridDimensions = gridDim;
+    //        gpuKernel.BlockDimensions = blockDim;
+    //    }
 
-        public override void GPUPreFrame()
-        {
-            cuDoubleComplex[] cuDoubles =
-                new cuDoubleComplex[X.Count];
-            for (var i = 0; i < cuDoubles.Length; i++)
-            {
-                GenericComplex<T> complex = X[i];
-                cuDoubles[i] = new cuDoubleComplex(
-                        TMath.toDouble(complex.real),
-                        TMath.toDouble(complex.imag));
-            }
+    //    public override void GPUPreFrame()
+    //    {
+    //        cuDoubleComplex[] cuDoubles =
+    //            new cuDoubleComplex[X.Count];
+    //        for (var i = 0; i < cuDoubles.Length; i++)
+    //        {
+    //            GenericComplex<T> complex = X[i];
+    //            cuDoubles[i] = new cuDoubleComplex(
+    //                    TMath.toDouble(complex.real),
+    //                    TMath.toDouble(complex.imag));
+    //        }
 
-            dev_points = cuDoubles;
-        }
+    //        dev_points = cuDoubles;
+    //    }
 
-        public override void GPUPostFrame()
-        {
-            dev_points.Dispose();
-        }
+    //    public override void GPUPostFrame()
+    //    {
+    //        dev_points.Dispose();
+    //    }
 
-        public override void GPUCell(
-            CudaDeviceVariable<int> dev_image,
-            CudaDeviceVariable<int> dev_palette,
-            int cell_x, int cell_y,
-            int cellWidth, int cellHeight,
-            int totalCells_x, int totalCells_y,
-            double xMax, double yMax,
-            int chunkSize, int maxChunkSize)
-        {
-            gpuKernel.Run(
-                dev_image.DevicePointer,
-                dev_palette.DevicePointer, dev_palette.Size,
-                dev_points.DevicePointer, dev_points.Size,
-                cell_x, cell_y,
-                cellWidth, cellHeight,
-                totalCells_x, totalCells_y,
-                xMax, yMax,
-                chunkSize, maxChunkSize);
-        }
+    //    public override void GPUCell(
+    //        CudaDeviceVariable<int> dev_image,
+    //        CudaDeviceVariable<int> dev_palette,
+    //        int cell_x, int cell_y,
+    //        int cellWidth, int cellHeight,
+    //        int totalCells_x, int totalCells_y,
+    //        double xMax, double yMax,
+    //        int chunkSize, int maxChunkSize)
+    //    {
+    //        gpuKernel.Run(
+    //            dev_image.DevicePointer,
+    //            dev_palette.DevicePointer, dev_palette.Size,
+    //            dev_points.DevicePointer, dev_points.Size,
+    //            cell_x, cell_y,
+    //            cellWidth, cellHeight,
+    //            totalCells_x, totalCells_y,
+    //            xMax, yMax,
+    //            chunkSize, maxChunkSize);
+    //    }
     }
 }
