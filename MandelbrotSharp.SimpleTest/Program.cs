31a6bae4b0e643412d06240e790b0c79329557dc
Yodadude2003 <yodadude2003@gmail.com>
2019-08-17 13:26:44 -0500

    Small changes to example application

--- MandelbrotSharp.SimpleTest/Program.cs
+++ MandelbrotSharp.SimpleTest/Program.cs
@@ -4,29 +4,44 @@ using MandelbrotSharp.Algorithms;
 using MandelbrotSharp.Imaging;
 using MandelbrotSharp.Numerics;
 using MandelbrotSharp.Rendering;
+using System.Buffers;
 
 namespace MandelbrotSharp.SimpleTest
 {
     class Program
     {
-        static DefaultRenderer<double, MandelbrotNAlgorithm<double>> Renderer =
-            new DefaultRenderer<double, MandelbrotNAlgorithm<double>>(32768, 32768);
-        static RgbaValue[] Colors = new RgbaValue[]
+        private static DefaultRenderer<double, MandelbrotAlgorithm<double>> Renderer =
+            new DefaultRenderer<double, MandelbrotAlgorithm<double>>(32768, 32768);
+        private static Gradient Colors = new Gradient(new RgbaValue[]
         {
-            new RgbaValue(0, 0, 0),
-            new RgbaValue(0, 0, 255),
-            new RgbaValue(0, 255, 0),
-            new RgbaValue(0, 0, 255)
-        };
+            new RgbaValue(9, 1, 47),
+            new RgbaValue(4, 4, 73),
+            new RgbaValue(0, 7, 100),
+            new RgbaValue(12, 44, 138),
+            new RgbaValue(24, 82, 177),
+            new RgbaValue(57, 125, 209),
+            new RgbaValue(134, 181, 229),
+            new RgbaValue(211, 236, 248),
+            new RgbaValue(241, 233, 191),
+            new RgbaValue(248, 201, 95),
+            new RgbaValue(255, 170, 0),
+            new RgbaValue(204, 128, 0),
+            new RgbaValue(153, 87, 0),
+            new RgbaValue(106, 52, 3),
+            new RgbaValue(66, 30, 15),
+            new RgbaValue(25, 7, 26),
+        }, 256);
 
         static void Main(string[] args)
         {
             Renderer.Setup(new RenderSettings
             {
+                OuterColors = Colors,
                 InnerColor = new RgbaValue(0, 0, 0),
-                OuterColors = new Gradient(Colors, 256),
+
                 ThreadCount = Environment.ProcessorCount,
-                Params = new MandelbrotNParams<double>
+
+                Params = new MandelbrotParams<double>
                 {
                     MaxIterations = 256,
                     EscapeRadius = 4.0,
@@ -44,10 +59,13 @@ namespace MandelbrotSharp.SimpleTest
         private static unsafe void FrameFinished(object sender, FrameEventArgs e)
         {
             SKBitmap bitmap = new SKBitmap(e.Frame.Width, e.Frame.Height, SKColorType.Bgra8888, SKAlphaType.Unpremul);
-            bitmap.SetPixels((IntPtr)e.Frame.CopyDataAsBits().AsMemory().Pin().Pointer);
-            SKFileWStream stream = new SKFileWStream("result.png");
+            MemoryHandle pixels = e.Frame.CopyDataAsBits().AsMemory().Pin();
+            bitmap.SetPixels((IntPtr)pixels.Pointer);
+            SKFileWStream stream = new SKFileWStream("output.png");
             SKPixmap.Encode(stream, bitmap, SKEncodedImageFormat.Png, 100);
             stream.Dispose();
+            bitmap.Dispose();
+            pixels.Dispose();
         }
     }
 }
