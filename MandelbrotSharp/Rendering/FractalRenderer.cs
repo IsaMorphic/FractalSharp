80bd4411f429d87a35db5890983ecfacd19fe1af
Yodadude2003 <yodadude2003@gmail.com>
2019-08-27 08:27:10 -0500

    Add copyright information

--- MandelbrotSharp/Rendering/FractalRenderer.cs
+++ MandelbrotSharp/Rendering/FractalRenderer.cs
@@ -15,6 +15,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with MandelbrotSharp.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 using MandelbrotSharp.Algorithms;
 using MandelbrotSharp.Data;
 using MandelbrotSharp.Imaging;
