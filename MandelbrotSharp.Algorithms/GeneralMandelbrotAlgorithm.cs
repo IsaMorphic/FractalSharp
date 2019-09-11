e00308248725bc25a20a3f007499739f47e4d0fc
Yodadude2003 <yodadude2003@gmail.com>
2019-09-11 14:05:18 -0500

    Add GeneralMandelbrotAlgorithm
    for any real or complex power, not just squaring

---
+++ MandelbrotSharp.Algorithms/GeneralMandelbrotAlgorithm.cs
@@ -0,0 +1,31 @@
+﻿using MandelbrotSharp.Data;
+using MandelbrotSharp.Numerics;
+
+namespace MandelbrotSharp.Algorithms
+{
+    public class GeneralMandelbrotParams : MTypeParams<double>
+    {
+        public Complex<double> Power { get; set; }
+
+        public override IAlgorithmParams Copy()
+        {
+            return new GeneralMandelbrotParams
+            {
+                MaxIterations = MaxIterations,
+                Magnification = Magnification,
+                Location = Location,
+
+                EscapeRadius = EscapeRadius,
+                Power = Power
+            };
+        }
+    }
+    public class GeneralMandelbrotAlgorithm :
+        MTypeAlgorithm<double, GeneralMandelbrotParams>
+    {
+        protected override Complex<double> DoIteration(Complex<double> z, Complex<double> c)
+        {
+            return CMath.Pow(z, Params.Power) + c;
+        }
+    }
+}
