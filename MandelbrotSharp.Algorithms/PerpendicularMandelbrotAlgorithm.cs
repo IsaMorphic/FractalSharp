07db83243efd289d6c0a19c1d280cb3214770c49
Yodadude2003 <yodadude2003@gmail.com>
2019-09-11 14:10:44 -0500

    Add PerpendicularMandelbrotAlgorithm
    yay! more algorithms!

---
+++ MandelbrotSharp.Algorithms/PerpendicularMandelbrotAlgorithm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MandelbrotSharp.Numerics;
+
+namespace MandelbrotSharp.Algorithms
+{
+    public class PerpendicularMandelbrotAlgorithm<TNumber> 
+        : SquareMandelbrotAlgorithm<TNumber>
+        where TNumber : struct
+    {
+        protected override Complex<TNumber> DoIteration(Complex<TNumber> z, Complex<TNumber> c)
+        {
+            var real = z.Real < Number<TNumber>.Zero ? -z.Real : z.Real;
+            var imag = new Complex<TNumber>(Number<TNumber>.Zero, z.Imag);
+            var y = real - imag;
+            return y * y + c;
+        }
+    }
+}
