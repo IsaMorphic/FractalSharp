595b3a66aa801eef9fd7b2d1ceeb8fa69738f586
Yodadude2003 <yodadude2003@gmail.com>
2020-01-08 13:27:06 -0600

    Rename example application project

--- FractalSharp.ExampleApp/Program.cs
+++ FractalSharp.ExampleApp/Program.cs
@@ -26,7 +26,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace FractalSharp.ConsoleTest
+namespace FractalSharp.ExampleApp
 {
     class SkiaImageBuilder : ImageBuilder
     {
