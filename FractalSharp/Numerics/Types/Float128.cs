0de9eb73c621415de0970916f8d5c2bd90ac3aed
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-08-25 16:29:18 -0500

    Add missing using

--- FractalSharp/Numerics/Types/Float128.cs
+++ FractalSharp/Numerics/Types/Float128.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
+using System.Linq;
 using System.Numerics;
 using System.Runtime.InteropServices;
 using System.Text;
