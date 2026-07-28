4888b0c1606a83fe73f1211f0093f1ee4b316eeb
Isabelle Santin <isabelle@chosenfewsoftware.com>
2026-07-28 12:37:59 -0500

    Fix: rename `RadialGradientParams` to `RadialGradientAlgoirthmParams`

--- FractalSharp.Algorithms/Coloring/RadialGradientAlgorithm.cs
+++ FractalSharp.Algorithms/Coloring/RadialGradientAlgorithm.cs
@@ -20,19 +20,19 @@ using FractalSharp.Numerics.Generic;
 
 namespace FractalSharp.Algorithms.Coloring
 {
-    public record struct RadialGradientParams
+    public record struct RadialGradientAlgorithmParams
     {
         public double Scale { get; set; }
 
-        public RadialGradientParams()
+        public RadialGradientAlgorithmParams()
         {
             Scale = 128;
         }
     }
 
-    public class RadialGradientAlgorithm : IAlgorithmProvider<PointData<double>, double, RadialGradientParams>
+    public class RadialGradientAlgorithm : IAlgorithmProvider<PointData<double>, double, RadialGradientAlgorithmParams>
     {
-        public static double Run(RadialGradientParams @params, PointData<double> data)
+        public static double Run(RadialGradientAlgorithmParams @params, PointData<double> data)
         {
             return @params.Scale * Complex<double>.Abs(data.ZValue).Real;
         }
